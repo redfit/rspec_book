504ac35ebce27cb06e2aab648d0b10b54b809cc8
redfit <daisuke.fuchiwaki@excel.co.jp>
2012-07-02 03:43:35 +0900

    codebreaker 17 p62

---
+++ cd/17/lib/codebreaker/game.rb
@@ -0,0 +1,13 @@
+module Codebreaker
+  class Game
+    def initialize(output)
+      @output = output
+    end
+
+    def start(secret=nil)
+      @output.puts 'Welcome to Codebreaker!'
+      @output.puts 'Enter guess:'
+    end
+  end
+end
+
