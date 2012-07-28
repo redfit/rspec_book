f32987f79b99238ac8794e3a5047876d39530513
daisuke fuchiwaki <daisuke.fuchiwaki@excel.co.jp>
2012-07-28 23:29:37 +0900

    cucumber example

---
+++ cucumber/02/features/room_steps.rb
@@ -0,0 +1,3 @@
+Given /^a hotel with "([^"]*)" rooms and "([^"]*)" bookings$/ do
+  |room_count, booking_count|
+end
---
+++ extending_rspec/network.rb
@@ -0,0 +1,33 @@
+
+#---
+# Excerpted from "The RSpec Book",
+# published by The Pragmatic Bookshelf.
+# Copyrights apply to this code. It may not be used to create training material,
+# courses, books, articles, and the like. Contact us if you are in doubt.
+# We make no guarantees that this code is fit for any purpose.
+# Visit http://www.pragmaticprogrammer.com/titles/achbd for more book information.
+#---
+# require 'ping'
+
+RSpec.configure do |c|
+  c.exclusion_filter = {
+    :if => lambda {|what|
+      case what
+      when :network_available
+        ret = `ping -c 1 #{example.com}`
+        if ret =~ /1 received/
+          true
+        else
+          false
+        end
+      end
+    }
+  }
+end
+
+describe "network group" do
+  it "example 1", :if => :network_available do
+  end
+  it "example 2" do
+  end
+end
